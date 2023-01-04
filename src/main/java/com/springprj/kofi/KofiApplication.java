67ba049fc20e086f61b4430d0889fe293b4431e9
abdelaliLaatamli <abdelaliLaatamli@gmail.com>
2023-01-04 08:06:14 +0100

    first commit

---
+++ src/main/java/com/springprj/kofi/KofiApplication.java
@@ -0,0 +1,13 @@
+package com.springprj.kofi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KofiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KofiApplication.class, args);
+	}
+
+}
